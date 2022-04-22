021a3115f7ec4127b839405c79f9f252d3168620
siddhantprateek <siddhantprateek@gmail.com>
2022-04-22 15:59:24 +0530

    🚩card component customized

--- src/renderer/components/card/card.components.tsx
+++ src/renderer/components/card/card.components.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import './card.styles.css';
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 
 const Card = (props: any) => {
   return (
     <div className="card-container">
       <img className="recent-music" src={props?.music_poster} alt="nice" />
-      <h3>{props.music_name}</h3>
-      <p>{props.artist}</p>
+      <br />
+      <PlayCircleFilledWhiteIcon className="play-btn-icon" fontSize="large" />
+      <div className="card-content">
+        <h3>{props.music_name}</h3>
+        <p>{props.artist}</p>
+      </div>
     </div>
   );
 };
