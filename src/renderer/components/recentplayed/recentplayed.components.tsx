b33d1a0e554447ea942e04b7192209b05778beb1
siddhantprateek <siddhantprateek@gmail.com>
2022-04-22 15:58:15 +0530

    🚩Recently view card

--- src/renderer/components/recentplayed/recentplayed.components.tsx
+++ src/renderer/components/recentplayed/recentplayed.components.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/destructuring-assignment */
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import './recentplayed.styles.css';
 
-const RecentPlayed = () => {
+const RecentPlayed = (props: any) => {
   return (
     <div className="recently-played">
-      <div className="icon" />
-      <p>Content</p>
+      <>
+        <img src={props.imgsrc} alt="" className="icon" />
+        <div className="recent-container-content">
+          <p>{props.content}</p>
+        </div>
+      </>
+      <PlayCircleFilledWhiteIcon className="play-btn-recent" fontSize="large" />
     </div>
   );
 };
--- src/renderer/routes/dashboard/dashboard.routes.tsx
+++ src/renderer/routes/dashboard/dashboard.routes.tsx
@@ -1,9 +1,45 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from 'renderer/components/card/card.components';
 import RecentPlayed from '../../components/recentplayed/recentplayed.components';
 import icon from '../../../../assets/icon.svg';
 import './dashboard.style.css';
+
+const RecentlyViewed = [
+  {
+    key: 1,
+    imgsrc: 'https://misc.scdn.co/liked-songs/liked-songs-300.png',
+    content: 'Liked Songs',
+  },
+  {
+    key: 2,
+    imgsrc: 'https://pbs.twimg.com/media/E7zJnI1VUAAA4Q7.jpg',
+    content: 'Discover Weekly',
+  },
+  {
+    key: 3,
+    imgsrc:
+      'https://geo-media.beatport.com/image_size/500x500/0a80304a-6f60-432f-ba9d-cf337498158c.jpg',
+    content: 'Out Loud',
+  },
+  {
+    key: 4,
+    imgsrc: 'https://i.scdn.co/image/ab6761610000e5ebc1fded2a185c54df399fe63a',
+    content: 'The Score',
+  },
+  {
+    key: 5,
+    imgsrc:
+      'https://i.pinimg.com/originals/bb/fd/53/bbfd535c644ac8a9f44ce5b9bfc9fc29.jpg',
+    content: 'One Republic',
+  },
+  {
+    key: 6,
+    imgsrc: 'https://wallpaperaccess.com/full/4341621.jpg',
+    content: 'Stronger',
+  },
+];
 
 const Dashboard = () => {
   const [musics, setMusics] = useState([]);
@@ -28,9 +64,9 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="recent-accessed-body">
-          {[...Array(6)].map((key) => (
-            <div key={key}>
-              <RecentPlayed />
+          {RecentlyViewed.map((data) => (
+            <div>
+              <RecentPlayed {...data} />
             </div>
           ))}
         </div>
