3611cc5e4efd8b5d98c82976b5871014b61830fb
siddhantprateek <siddhantprateek@gmail.com>
2022-04-22 15:58:49 +0530

    🚩side css padding to list

--- src/renderer/components/sidebar/sidebar.components.tsx
+++ src/renderer/components/sidebar/sidebar.components.tsx
@@ -2,6 +2,8 @@ import './status.styles.css';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
 import LikeMusic from '../../../../assets/like-music-icon.png';
 
 const SideBar = () => {
@@ -10,19 +12,28 @@ const SideBar = () => {
       <ul className="nav-option">
         <li>
           <HomeIcon />
-          Home
+          <p>Home</p>
         </li>
         <li>
           <SearchIcon />
-          Search
+          <p>Search</p>
         </li>
         <li>
           <LibraryMusicIcon />
-          Your Library
+          <p>Your Library</p>
+        </li>
+        <br />
+        <li>
+          <AddBoxIcon />
+          <p>Create Playlist</p>
         </li>
         <li>
           <img className="side-icons" src={LikeMusic} alt="" />
-          Liked Songs
+          <p>Liked Songs</p>
+        </li>
+        <li>
+          <PodcastsIcon />
+          <p>Your Episodes</p>
         </li>
       </ul>
     </div>
