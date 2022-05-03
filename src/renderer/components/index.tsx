8fe8a2225dc7e976289677076255377ce43d690d
siddhantprateek <siddhantprateek@gmail.com>
2022-05-03 11:43:11 +0530

    🚩default export added

---
+++ src/renderer/components/index.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Player } from './player/player.components';
+export { default as SideBar } from './sidebar/sidebar.components';
+export { default as Activity } from './activity/activity.components';
---
+++ src/renderer/routes/index.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Dashboard } from './dashboard/dashboard.routes';
+export { default as Library } from './library/library.routes';
+export { default as Search } from './search/search.routes';
